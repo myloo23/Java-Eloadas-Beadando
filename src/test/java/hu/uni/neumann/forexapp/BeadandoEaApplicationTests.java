fb01f2d284d45e66761857d1f38a5d85d2826a87
Turikrisztian <148880706+Turikrisztian@users.noreply.github.com>
2025-11-20 18:34:53 +0100

    3

---
+++ src/test/java/hu/uni/neumann/forexapp/BeadandoEaApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.uni.neumann.forexapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeadandoEaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
